f5489750d9da09ffd58053f0b208bc2130a9d0d2
song248 <songteagyong@gmail.com>
2021-04-25 15:26:51 +0900

    0425_commit_1

--- 송태경/04M04W/백준/7.문자열/1316_그룹 단어 체커.py
+++ 송태경/04M04W/백준/7.문자열/1316_그룹 단어 체커.py
@@ -34,3 +34,18 @@ a
 예제 출력 2 
 1
 '''
+
+t = int(input())
+cnt  = 0
+
+for _ in range(t):
+    s =  input()
+    for i in range(len(s)):
+        if i != len(s)-1:
+            if s[i] == s[i+1]:
+                pass
+            elif s[i] in s[i+1:]:
+                break
+        else:
+            cnt += 1
+print(cnt)
