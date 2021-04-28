7abe7de2b98d97b6488c565566642822610347ff
unknown <OT21035@oad.kcb4u.dom>
2021-04-28 16:09:08 +0900

    0428_commit_2

--- 송태경/04M05W/백준/11.브루트 포스/2231_분해합.py
+++ 송태경/04M05W/백준/11.브루트 포스/2231_분해합.py
@@ -8,3 +8,18 @@ for i in range(1, int(n)+1):
         sol = i
         break
 print(sol)
+
+
+n = int(input())
+result = 0
+
+for i in range(1, n+1):        
+    a = list(map(int, str(i)))  
+    s = i + sum(a)              
+    if(s == n):                 
+        result = i                   
+        break
+
+print(result)
+
+# 탐색 범위를 어떻게 설정하느냐에 따라 실행속도 더 줄이기 가능
