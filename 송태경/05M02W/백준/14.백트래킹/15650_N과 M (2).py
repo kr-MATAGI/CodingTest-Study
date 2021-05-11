302aa53bd9313ae7342dfae753bc3ddd05851ef4
song248 <songteagyong@gmail.com>
2021-05-11 23:17:45 +0900

    0511_commit_1

--- 송태경/05M02W/백준/14.백트래킹/15650_N과 M (2).py
+++ 송태경/05M02W/백준/14.백트래킹/15650_N과 M (2).py
@@ -1,14 +1,9 @@
-'''
-백트래킹(퇴각검색)
-DFS(깊이 우선 탐색) 방식 기반으로, 불필요한 경우를 배제하며
-원하는 해답에 도달할 때까지 탐색하는 전략
-'''
-
 n, m = map(int, input().split())
 
 number_list = [1 + i for i in range(n)]
 check_number = [False] * n
 array = []
+
 def back(x):
     if x == m:
         print(' '.join(map(str, array)))            
@@ -22,6 +17,7 @@ def back(x):
 
         back(x + 1)
 
-        array.pop()         
-        check_number[i] = False
+        array.pop() 
+        for j in range(i+1, n):
+            check_number[j] = False        
 back(0)    
