f3938c6227d315ebbe8579c620603500124ce8db
song248 <songteagyong@gmail.com>
2021-08-05 14:15:36 +0900

    0805_commit_1

---
+++ 송태경/08M/programmers/level2/큰 수 만들기.py
@@ -0,0 +1,22 @@
+def solution(number, k):
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
+	
+	
+def solution(number, k):
+    stack = []
+    for num in number:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
